eba8af6b43b8da47e6c360b8b6492f6c21ccf1d4
Artem <artem@MacBook-Pro-Artem.local>
2021-05-24 10:41:13 +0300

    add coupon form

--- src/js/validation/index.js
+++ src/js/validation/index.js
@@ -2,10 +2,26 @@ import pristine from 'pristinejs/dist/pristine';
 
 // subscription //
 const subscriptionForm = document.querySelector('#form_5');
-const subscription = new pristine(subscriptionForm);
+let subscription;
+if(!!subscriptionForm) {
+    subscription = new pristine(subscriptionForm);
+}
 
 // subscriptionForm.addEventListener('submit', function (event) {
 //     event.preventDefault();
 //     let isSubscriptionValid = subscription.validate();
 //     console.log(isSubscriptionValid);
+// });
+
+// coupon //
+const couponForm = document.querySelector('#coupon-form');
+let coupon;
+if(!!couponForm) {
+    coupon = new pristine(couponForm);
+}
+
+// couponForm.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     let isCouponValid = coupon.validate();
+//     console.log(isCouponValid);
 // });
