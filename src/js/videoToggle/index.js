245b9f7f554063220660686c2856307ca9f9952d
Artem <artem@MacBook-Pro-Artem.local>
2021-05-26 14:04:11 +0300

    optimaze youtube iframe load

--- src/js/videoToggle/index.js
+++ src/js/videoToggle/index.js
@@ -1,8 +1,14 @@
 import inView from 'in-view/dist/in-view.min';
 
 inView('#player')
-    .on('enter', function () {
-        controlVideo('playVideo');
+    .on('enter', function (elem) {
+        if(checkDataSrc(elem)) {
+            setTimeout(function () {
+                controlVideo('playVideo');
+            }, 1000);
+        } else {
+            controlVideo('playVideo');
+        }
     })
     .on('exit', function () {
         controlVideo('pauseVideo');
@@ -12,4 +18,15 @@ function controlVideo(vidcontrol) {
     const div = document.getElementById("player");
     const iframe = div.getElementsByTagName("iframe")[0].contentWindow;
     iframe.postMessage('{"event":"command","func":"' + vidcontrol + '","args":""}', '*');
+}
+
+function checkDataSrc (elem) {
+    const iframe = elem.getElementsByTagName("iframe")[0];
+    const dataSrc = iframe.getAttribute('data-src');
+    if(!!dataSrc) {
+        iframe.setAttribute('src', dataSrc);
+        iframe.removeAttribute('data-src');
+        return true;
+    }
+    return false;
 }
