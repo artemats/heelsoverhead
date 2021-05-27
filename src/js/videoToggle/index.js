846f4cf1aff4e1cfa6c60c8159c59bd3efe35860
Artem <artem@MacBook-Pro-Artem.local>
2021-05-27 11:24:10 +0300

    fix video player

--- src/js/videoToggle/index.js
+++ src/js/videoToggle/index.js
@@ -1,17 +1,25 @@
 import inView from 'in-view/dist/in-view.min';
+import Plyr from "plyr";
+
+const player = new Plyr(document.querySelector('#player'), {
+    controls: []
+});
 
 inView('#player')
     .on('enter', function (elem) {
         if(checkDataSrc(elem)) {
             setTimeout(function () {
                 controlVideo('playVideo');
+                player.play();
             }, 1000);
         } else {
             controlVideo('playVideo');
+            player.play();
         }
     })
     .on('exit', function () {
         controlVideo('pauseVideo');
+        player.pause();
     });
 
 function controlVideo(vidcontrol) {
