c944f4379f4cabd2a10840d26c77f11a2905a046
Artem <artem@MacBook-Pro-Artem.local>
2021-05-28 17:27:38 +0300

    test html5 video

--- src/js/videoToggle/index.js
+++ src/js/videoToggle/index.js
@@ -1,24 +1,31 @@
 import inView from 'in-view/dist/in-view.min';
 import Plyr from "plyr";
 
-const player = new Plyr(document.querySelector('#player'), {
-    controls: ['play-large']
-});
+// const player = new Plyr(document.querySelector('#player'), {
+//     controls: ['play-large']
+// });
 
-const btnPlayPause = document.querySelector('.plyr__control');
+// const btnPlayPause = document.querySelector('.plyr__control');
+const video = document.querySelector('#player');
 
 inView('#player')
     .on('enter', function () {
         // controlVideo('playVideo');
-        if(!player.play()) {
-            btnPlayPause.click();
-        } else {
-            player.play();
-        }
+        // if(!player.play()) {
+        //     btnPlayPause.click();
+        // } else {
+        //     player.play();
+        // }
+        video.play();
+        // setTimeout(function () {
+        //     video.muted = false;
+        // }, 1000);
     })
     .on('exit', function () {
         // controlVideo('pauseVideo');
-        player.pause();
+        // player.pause();
+        video.pause();
+        // video.muted = true;
     });
 
 function controlVideo(vidcontrol) {
